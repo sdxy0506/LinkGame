d59b7e9e85a8063a4b7a32c3f4bf61c5fd15aa2c
xuyan <xuyan>
2013-04-16 19:53:12 +0800

    v0.1 LinkGame
    成对随机生成图块

---
+++ LinkGame/gen/com/xuyan/linkgame/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.xuyan.linkgame;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LinkGame/src/com/xuyan/linkgame/MyView.java
@@ -0,0 +1,99 @@
+package com.xuyan.linkgame;
+
+import java.util.Random;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
+import android.app.Activity;
+import java.util.*;
+
+public class MyView extends View {
+	public MyView(Context context,AttributeSet set)
+	{
+		super(context,set);
+	}
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		//Context context;
+		//int screenWidth=((Activity)context).getWindowManager().getDefaultDisplay().getWidth();
+		super.onDraw(canvas);
+		canvas.drawColor(Color.WHITE);
+		Paint paint=new Paint();
+		int[] image=new int[]{R.drawable.a,
+				              R.drawable.b,
+				              R.drawable.c,
+				              R.drawable.d,
+				              R.drawable.e,
+				              R.drawable.f,
+				              R.drawable.g,
+				              R.drawable.h,
+				              R.drawable.i,
+				              R.drawable.j,
+				              R.drawable.k,};
+		//声明一个6*6的二维数组，用来存放36张图片的相对位置
+		int[][] array=new int[6][6];
+		int n=0;
+		for(int i=0;i<6;i++)
+		{
+			for(int j=0;j<6;j++)
+			{
+				array[i][j]=++n;
+			}
+		}
+		//生成当前系统时间的秒
+		//Calendar ca=Calendar.getInstance();
+		//int seed=ca.get(Calendar.SECOND)%30;
+		Random random1=new Random(1);
+		Random random2=new Random(2);
+		Random random3=new Random(3);
+		Random random4=new Random(3);
+		int k=0;
+		int stop=0;
+		while(stop!=36)
+		{
+			int point1=random1.nextInt(6);
+			int point2=random2.nextInt(6);
+			int point3=random3.nextInt(6);
+			int point4=random4.nextInt(6);
+			Bitmap bitmap=BitmapFactory.decodeResource(this.getContext().getResources(), image[k]);
+			if(array[point1][point2]==0||array[point3][point4]==0)
+				stop=stop;
+			else 
+			{
+				canvas.drawBitmap(bitmap, point1*100, point2*100, paint);
+				canvas.drawBitmap(bitmap, point3*100, point4*100, paint);
+				array[point1][point2]=0;
+				array[point3][point4]=0;
+				stop=stop+2;
+			}
+			/*if(array[point1][point2]!=0&&array[point3][point4]!=0)
+			{
+				canvas.drawBitmap(bitmap, point1*100, point2*100, paint);
+				canvas.drawBitmap(bitmap, point3*100, point4*100, paint);
+				array[point1][point2]=0;
+				array[point3][point4]=0;
+				stop=stop+2;
+			}*/
+			k=(k+1)%7;
+			//canvas.drawBitmap(bitmap, point2*100, point2*100, paint);
+		}
+		/*for(int i=1;i<700;i=i+100)
+		{
+			for(int j=1;j<800;j=j+100)
+			{
+				canvas.drawBitmap(bitmap, i, j, paint);
+
+			}
+		}*/
+     }
+
+}
