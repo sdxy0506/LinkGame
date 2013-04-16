2d6e6cfba33b7f368e535ecce7ea987ae03779b3
xuyan <xuyan>
2013-04-16 23:50:37 +0800

    v0.2增加选中图块时的圈中效果

--- LinkGame/src/com/xuyan/linkgame/MyView.java
+++ LinkGame/src/com/xuyan/linkgame/MyView.java
@@ -10,12 +10,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.app.Activity;
 import java.util.*;
 
 public class MyView extends View {
+	public float currentX,currentY;
 	public MyView(Context context,AttributeSet set)
 	{
 		super(context,set);
@@ -23,8 +25,6 @@ public class MyView extends View {
 	@Override
 	protected void onDraw(Canvas canvas)
 	{
-		//Context context;
-		//int screenWidth=((Activity)context).getWindowManager().getDefaultDisplay().getWidth();
 		super.onDraw(canvas);
 		canvas.drawColor(Color.WHITE);
 		Paint paint=new Paint();
@@ -55,7 +55,7 @@ public class MyView extends View {
 		Random random1=new Random(1);
 		Random random2=new Random(2);
 		Random random3=new Random(3);
-		Random random4=new Random(3);
+		Random random4=new Random(4);
 		int k=0;
 		int stop=0;
 		while(stop!=36)
@@ -65,9 +65,7 @@ public class MyView extends View {
 			int point3=random3.nextInt(6);
 			int point4=random4.nextInt(6);
 			Bitmap bitmap=BitmapFactory.decodeResource(this.getContext().getResources(), image[k]);
-			if(array[point1][point2]==0||array[point3][point4]==0)
-				stop=stop;
-			else 
+			if(array[point1][point2]!=0&&array[point3][point4]!=0)
 			{
 				canvas.drawBitmap(bitmap, point1*100, point2*100, paint);
 				canvas.drawBitmap(bitmap, point3*100, point4*100, paint);
@@ -75,14 +73,6 @@ public class MyView extends View {
 				array[point3][point4]=0;
 				stop=stop+2;
 			}
-			/*if(array[point1][point2]!=0&&array[point3][point4]!=0)
-			{
-				canvas.drawBitmap(bitmap, point1*100, point2*100, paint);
-				canvas.drawBitmap(bitmap, point3*100, point4*100, paint);
-				array[point1][point2]=0;
-				array[point3][point4]=0;
-				stop=stop+2;
-			}*/
 			k=(k+1)%7;
 			//canvas.drawBitmap(bitmap, point2*100, point2*100, paint);
 		}
@@ -94,6 +84,24 @@ public class MyView extends View {
 
 			}
 		}*/
+		int rect_x=(int)currentX-((int)currentX)%100;
+		int rect_y=(int)currentY-((int)currentY)%100;
+		Paint paint_rect=new Paint();
+		paint_rect.setAntiAlias(true);
+		paint_rect.setColor(Color.RED);
+		paint_rect.setStyle(Paint.Style.STROKE);
+		paint_rect.setStrokeWidth(8);
+		Bitmap bitmap=BitmapFactory.decodeResource(this.getContext().getResources(), image[0]);
+		int img_width=bitmap.getWidth();
+		canvas.drawRect(rect_x, rect_y, rect_x+img_width, rect_y+img_width, paint_rect);		
+	}
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		currentX=event.getX();
+		currentY=event.getY();
+		invalidate();
+		return true;
+	}
      }
 
-}
